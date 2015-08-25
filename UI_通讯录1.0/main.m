b1f362abcf5650a163da7a5b20954d2d775c1a1f
邱文辉 <jacky@qiuwenhuideMacBook-Air.local>
2015-08-25 18:01:58 +0800

    Initial Commit

---
+++ UI_通讯录1.0/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UI_通讯录1.0
+//
+//  Created by 邱文辉 on 15/8/25.
+//  Copyright (c) 2015年 邱文辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
